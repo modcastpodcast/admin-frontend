be6be23071fb0c856181435e15d52c2ec16a1765
Joseph Banks <joseph@josephbanks.me>
2020-08-07 20:00:52 +0100

    Add user row

---
+++ src/components/user/index.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import moment from "moment";
+
+import "./user.scss";
+
+import { User } from "../../types";
+import ProfilePicture from "../profile_picture";
+
+interface UserProps {
+    user: User
+}
+
+class UserRow extends Component<UserProps, {}> {
+    render() {
+        return <div className="User">
+            <div className="creator">
+                <ProfilePicture id={this.props.user.id} avatar={this.props.user.avatar} size={60} discordSize={128}/>
+                <h2 className="username">{this.props.user.username}#{this.props.user.discriminator}</h2>
+                <p className="administrator">{this.props.user.api_key!.is_admin ? "Administrator" : ""}</p>
+            </div>
+        </div>
+    }
+}
+
+export default UserRow;
