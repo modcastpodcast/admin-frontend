cd8038e6cce0c729c533b9ab9a45b40b2cd4a95b
Joseph Banks <joseph@josephbanks.me>
2020-08-07 20:32:01 +0100

    Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 import App from './app/App';
 import "./index.scss";
 import * as serviceWorker from './serviceWorker';
+
+if (process.env.NODE_ENV == "production") {
+  Sentry.init({dsn: "https://730b85d36361482f862e3fb0ed12c2db@o431389.ingest.sentry.io/5382334"});
+}
 
 ReactDOM.render(
   <React.StrictMode>
