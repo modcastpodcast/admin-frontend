e045aba306d951ff2b51c6d1c53790c8205ac820
Joseph Banks <joseph@josephbanks.me>
2020-08-07 04:23:10 +0100

    Add more API methods

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,11 @@
+import {User} from "./types";
+
 const API_BASE = "https://modpod.live/api";
 const OAUTH2_AUTHORIZE = "https://modpod.live/oauth2/authorize";
 
-function redirectToAuthorize() {
+const USER_CACHE: { [id: string] : User } = {}
+
+export function redirectToAuthorize() {
     document.location.href = OAUTH2_AUTHORIZE;
 }
 
@@ -15,10 +19,32 @@ async function get(route: string) {
             })
         } catch(e) {
             redirectToAuthorize();
+            throw 403;
         }
+    } else {
+        redirectToAuthorize();
+        throw 403;
     }
 }
 
-export async function getUser() {
-    fetch("")
+export async function getUser(userID: string): Promise<User> {
+    if (USER_CACHE[userID]) {
+        return new Promise((resolve) => {
+            resolve(USER_CACHE[userID]);
+        })
+    };
+
+    let user = await get(`/users/${userID}`);
+
+    let user_data = await user.json();
+
+    return user_data;
+}
+
+export async function getCurrentUser() {
+    let user = await get("/me");
+
+    let currentToken = await user.json();
+
+    return currentToken;
 }
