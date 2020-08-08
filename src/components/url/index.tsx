5ea4bb167d3bcf266cdb9bfd27333576e9a71ffe
Joseph Banks <joseph@josephbanks.me>
2020-08-08 08:18:11 +0100

    Add edit icon and edit button

--- src/components/url/index.tsx
+++ src/components/url/index.tsx
@@ -7,12 +7,15 @@ import { Link, APIKey } from "../../types";
 import ProfilePicture from "../profile_picture";
 
 import trash from "./trash.svg";
+import edit from "./edit.svg";
+
 import { wrapPromise, WrappedPromise } from "../../utils";
 import { getCurrentUser, deleteShortURL } from "../../api";
 
 interface URLProps  {
     link: Link,
-    setRerender: () => any
+    setRerender: () => any,
+    makeActive: (url: Link) => any
 }
 
 const currentUser: WrappedPromise<APIKey> = wrapPromise(getCurrentUser());
@@ -22,6 +25,7 @@ class URL extends Component<URLProps, {}> {
         super(props);
 
         this.deleteLink = this.deleteLink.bind(this);
+        this.editLink = this.editLink.bind(this);
     }
 
     formatCreated() {
@@ -38,15 +42,22 @@ class URL extends Component<URLProps, {}> {
         deleteShortURL(this.props.link.short_code).then(() => this.props.setRerender());
     }
 
+    editLink() {
+        this.props.makeActive(this.props.link);
+    }
+
     render() {
-        let deleteButton;
+        let actionButtons;
 
         if (this.props.link.creator.id === currentUser.read().creator || currentUser.read().is_admin) {
-            deleteButton = <button onClick={this.deleteLink} className="deleteButton"><img alt="delete short url" src={trash} width={20}/></button>;
+            actionButtons = <div className="actions">
+                <button onClick={this.deleteLink} className="deleteButton"><img alt="delete short url" src={trash} width={20}/></button>
+                <button onClick={this.editLink} className="deleteButton"><img alt="delete short url" src={edit} width={20}/></button>
+            </div>;
         }
 
         return <div className="URL">
-            {deleteButton}
+            {actionButtons}
             <h2 className="shortCode">{this.props.link.short_code}</h2>
             <p className="longURL">Points to <a href={this.props.link.long_url}>{this.props.link.long_url}</a></p>
             <p className="creator">Created by <ProfilePicture id={this.props.link.creator.id} avatar={this.props.link.creator.avatar} size={32} discordSize={64}/> <strong>{this.props.link.creator.username}#{this.props.link.creator.discriminator}</strong></p>
