ba17140ec8a95cb29e7244f0f9a3c990f01eb124
Joe Banks <joseph@josephbanks.me>
2020-09-20 00:46:15 +0100

    🐛 [component/CurrentUser]: Move user fetch to state of component

--- src/components/current_user/index.tsx
+++ src/components/current_user/index.tsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./current_user.scss";
 
 import { getCurrentUser, getUser } from "../../api";
 import ProfilePicture from "../profile_picture";
-import {wrapPromise} from "../../utils";
+import {WrappedPromise, wrapPromise} from "../../utils";
+import { User } from "../../types";
 
-const userDataRequest = wrapPromise((async () => {
-    let currentKey = await getCurrentUser();
-    let currentUser = await getUser(currentKey.creator);
+interface CurrentUserState {
+    dataRequest: WrappedPromise<User>
+}
 
-    return currentUser;
-})());
+class CurrentUser extends Component<Readonly<{}>, CurrentUserState> {
+    constructor(props: Readonly<{}>) {
+        super(props);
 
-function CurrentUser() {
+        const dataRequest = wrapPromise((async () => {
+            let currentKey = await getCurrentUser();
+            let currentUser = await getUser(currentKey.creator);
+        
+            return currentUser;
+        })())
 
-    const userData = userDataRequest.read();
+        this.state = {
+            dataRequest,
+        }
+    }
 
-    return <div className="CurrentUser">
-        <ProfilePicture id={userData.id} avatar={userData.avatar} size={60} discordSize={128}/>
-        <p className="username">Logged in as {userData.username}</p>
-    </div>
+    render() {
+        const userData = this.state.dataRequest.read();
+
+        return <div className="CurrentUser">
+            <ProfilePicture id={userData.id} avatar={userData.avatar} size={60} discordSize={128}/>
+            <p className="username">Logged in as {userData.username}</p>
+        </div>
+    }
 }
 
 export default CurrentUser;
